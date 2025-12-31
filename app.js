9bda4b38e0edf1e8603c9200e63d34847a449f49
vaibhavrandale <127591217+vaibhavrandale@users.noreply.github.com>
2025-12-31 18:06:22 +0530

    Add API endpoints for GET and POST requests

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 const app = express();
 dotenv.config();
-app.get("/", (req, res) => {
+app.get("/api/get", (req, res) => {
   res.send("Welcom to nodejs deployment");
+});
+
+app.get("/api/post", (req, res) => {
+  res.send("nodejs posted successfully!");
 });
 
 app.listen(process.env.PORT, () => {
