45b75db9d0aa415e8e774c48685f0417c71bfe50
vaibhavrandale <127591217+vaibhavrandale@users.noreply.github.com>
2025-12-31 18:14:27 +0530

    Fix: updated backend logic and UI changes

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 app.get("/api/get", (req, res) => {
-  res.send("Welcom to nodejs deployment");
+  res.send({ message: "Welcom to nodejs deployment" });
 });
 
 app.get("/api/post", (req, res) => {
-  res.send("nodejs posted successfully!");
+  res.send({ message: "nodejs posted successfully!" });
 });
 
 app.listen(9000, () => {
